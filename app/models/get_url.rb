4ad92de51c0a03831de3e5cc95a2e5f8018217e2
Jamie Brough <jamie1612@gmail.com>
2017-11-23 10:27:32 +0000

    handle redirects

--- app/models/get_url.rb
+++ app/models/get_url.rb
@@ -2,21 +2,51 @@ require 'ougai'
 
 class GetURL
   Logger = Ougai::Logger.new(STDOUT)
+  UA = 'Mozilla/5.0 (X11; CrOS x86_64 9901.77.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.97 Safari/537.36'
 
   # TODO: pass in a correlation ID for logging
   def self.do(uri)
     t = Time.now
-    req = Net::HTTP::Get.new(uri.to_s)
-    res = Net::HTTP.start(uri.host, uri.port) {|http|
-        http.request(req)
-    }
+
+		res = nil
+  	tries = 0
+
+		uri_string = uri.to_s
+
+		loop do
+			uri = URI.parse(uri_string)
+    	req = Net::HTTP::Get.new(uri.to_s, {'User-Agent': UA})
+			http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = (uri.scheme == 'https')
+      res = http.request(req)
+
+      if res['location']
+    	  uri_string = res['location']
+        if uri_string[0] == '/'
+          uri.path = uri_string
+          uri_string = uri.to_s
+        end
+      end
+
+      if !res.kind_of? Net::HTTPRedirection
+      	break
+    	end
+
+    	if tries == 10
+      	break
+    	end
+
+    	tries += 1
+  	end
 
     msg = {
       code: res.code,
       method: "GET",
       msec: ((Time.now - t) * 1000.0).to_i,
       url: uri.to_s,
-    }
+      redirects: tries,
+	  	redirects_exceeded: tries == 10,
+		}
 
     if res.code > '308'
       Logger.error(msg)
