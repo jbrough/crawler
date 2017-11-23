5391f4e3124fe5112670ede4ef568b7b52c7bf94
Jamie Brough <jamie1612@gmail.com>
2017-11-23 10:04:12 +0000

    use Net::HTTP rather than open-uri and log request

--- app/models/get_url.rb
+++ app/models/get_url.rb
@@ -1,5 +1,29 @@
+require 'ougai'
+
 class GetURL
+  Logger = Ougai::Logger.new(STDOUT)
+
+  # TODO: pass in a correlation ID for logging
   def self.do(uri)
-    open(uri.to_s).read
+    t = Time.now
+    req = Net::HTTP::Get.new(uri.to_s)
+    res = Net::HTTP.start(uri.host, uri.port) {|http|
+        http.request(req)
+    }
+
+    msg = {
+      code: res.code,
+      method: "GET",
+      msec: ((Time.now - t) * 1000.0).to_i,
+      url: uri.to_s,
+    }
+
+    if res.code > '308'
+      Logger.error(msg)
+    else
+      Logger.info(msg)
+    end
+
+    res.body
   end
 end
