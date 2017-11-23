10a0fab90a03f80d63d287afc666ae425d73e1c1
Jamie Brough <jamie1612@gmail.com>
2017-11-23 05:40:37 +0000

    AlexaParser implementation for extracting country %

--- app/models/alexa_parser.rb
+++ app/models/alexa_parser.rb
@@ -1,8 +1,19 @@
 class AlexaParser
+
+  Stat = Struct.new(:country, :percent)
+
   def initialize(html_doc)
+    @doc = Nokogiri::HTML(html_doc)
   end
 
   def country_stats
-    []
+    node = @doc.at_css('#demographics_div_country_table tbody')
+    node.css('tr').map do |tr|
+      Stat.new(
+        tr.at_css('a')['href'].split('/')[3],
+        tr.at_css('span').content.to_f,
+      )
+    end
   end
+
 end
